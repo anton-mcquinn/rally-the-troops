0ac85ed57e891fda0cc83d8709145c245f36f027
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-09-01 16:58:22 -0700

    gotta remove that hard-coded db access

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,22 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        // Replace <username>, <password>, <database_name> with your actual credentials
-        const mongoURI = 'mongodb://test:test@127.0.0.1:27017/rally-the-troops?authSource=rally-the-troops';
+  try {
+    // Use the MONGO_URI from the environment variables
+    const mongoURI = process.env.MONGO_URI as string;
 
-const conn = await mongoose.connect(mongoURI);
+    const conn = await mongoose.connect(mongoURI);
 
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error(err.message);
-            process.exit(1); // Exit process with failure
-        } else {
-            console.error('An unexpected error occurred');
-            process.exit(1); // Exit process with failure
-        }
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      process.exit(1); // Exit process with failure
+    } else {
+      console.error("An unexpected error occurred");
+      process.exit(1); // Exit process with failure
     }
+  }
 };
 
 export default connectDB;
