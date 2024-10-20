49c187d9bf9572946e2b7c7d8115e9d1eee1e457
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-10-20 12:28:58 -0700

    squad schema fix

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,18 +2,16 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
   username: string;
-  _id: mongoose.Types.ObjectId;
   email: string;
   password: string;
-  squad: mongoose.Schema.Types.ObjectId[];
+  squad: mongoose.Schema.Types.ObjectId[]; // Array of User ObjectIds
 }
 
 const UserSchema: Schema<IUser> = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  squad: [{ type: mongoose.Schema.Types.ObjectId, ref: "Squad" }],
+  squad: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Array of User references
 });
 
-const User = mongoose.model<IUser>("User", UserSchema);
-export default User;
+export default mongoose.model<IUser>("User", UserSchema);
