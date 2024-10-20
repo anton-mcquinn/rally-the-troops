f21cb9d18610d447b102744c1a45e9fe51879719
Anton McIntosh <96960578+anton-mcintosh@users.noreply.github.com>
2024-10-20 12:32:41 -0700

    should be more careful when copying/pasting

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,16 +2,18 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
   username: string;
+  _id: mongoose.Types.ObjectId;
   email: string;
   password: string;
-  squad: mongoose.Schema.Types.ObjectId[]; // Array of User ObjectIds
+  squad: mongoose.Schema.Types.ObjectId[];
 }
 
 const UserSchema: Schema<IUser> = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  squad: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Array of User references
+  squad: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
-export default mongoose.model<IUser>("User", UserSchema);
+const User = mongoose.model<IUser>("User", UserSchema);
+export default User;
